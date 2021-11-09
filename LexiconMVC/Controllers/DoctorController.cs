704faef5519fd1f750d9fdc0e27c5172fd9a9c05
richard sandblom <r.sandblom@gmail.com>
2021-11-09 11:43:41 +0100

    Github troubles

---
+++ LexiconMVC/Controllers/DoctorController.cs
@@ -0,0 +1,37 @@
+﻿using LexiconMVC.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LexiconMVC.Controllers
+{
+    public class DoctorController : Controller
+    {
+        public ActionResult Index()
+        {
+            
+            string resultMsg = HttpContext.Session.GetString("TempCheckResult");
+            if(resultMsg == null || resultMsg == "")
+                ViewBag.Msg = null;
+            else
+                ViewBag.Msg = resultMsg;
+
+            return View();
+        }
+
+        
+        [HttpPost]
+        public ActionResult Index(Temperature temperature)
+        {
+            string bodyStatusMsg = TemperatureEvaluator.GetBodyTemperatureStatus(temperature);
+            HttpContext.Session.SetString("TempCheckResult", bodyStatusMsg);
+            
+            return RedirectToAction(nameof(Index));
+        }
+
+        
+    }
+}
---
+++ LexiconMVC/Models/TemperatureEvaluator.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LexiconMVC.Models
+{
+    public static class TemperatureEvaluator
+    {
+        public static string GetBodyTemperatureStatus(Temperature temp)
+        {
+            if (temp.CurrentTemp == 0)
+                return "";
+
+
+            double fever, hypothermia;
+
+            if (temp.IsCelsius)
+            {
+                fever = 38;
+                hypothermia = 35;
+            }
+            else
+            {
+                fever = 100.4;
+                hypothermia = 95;
+            }
+
+            if(temp.CurrentTemp < hypothermia)
+            {
+                return "You have hypothermia";
+            }
+            if (temp.CurrentTemp > fever)
+            {
+                return "You have e fever";
+            }
+            else
+            {
+                return "Your body temperature is ok";
+            }
+           
+        }
+    }
+}
